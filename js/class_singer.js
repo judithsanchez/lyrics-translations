247f9e64588ae8e7f237ebef9987b388c8b7ff4a
Judith Sanchez <judithv.sanchezc@gmail.com>
2023-01-07 01:23:15 -0500

    Created my project.

---
+++ js/class_singer.js
@@ -0,0 +1,9 @@
+class Singer {
+  constructor({ name, nationality, spotify, youtube, wikipedia }) {
+    this.name = name;
+    this.nationality = nationality;
+    this.spotify = spotify;
+    this.youtube = youtube;
+    this.wikipedia = wikipedia;
+  }
+}
